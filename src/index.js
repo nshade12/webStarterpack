210bac1fb55d9d700331d0e9b81185202b8bc6c7
nshade12 <nicholas.r.shade.20@dartmouth.edu>
2019-06-14 02:24:50 -0500

    html, js, sass, css all working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('starting up!');
+import $ from 'jquery';
+import './style.scss';
+
+let count = 0;
+
+setInterval(() => {
+  $('#main').html(`You've been on this page for ${count} seconds.`);
+  count += 1;
+}, 1000);
