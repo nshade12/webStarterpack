701e65a50f0bec207a45a80cfb8344c79705625a
nshade12 <nicholas.r.shade.20@dartmouth.edu>
2019-08-21 23:13:45 -0700

    react router setup and deployed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router, Route, NavLink, Switch,
+} from 'react-router-dom';
 import './style.scss';
 
-const App = () => <div className="test">All the REACT are belong to us!</div>;
+const About = (props) => {
+  return <div> All there is to know about me </div>;
+};
+
+const Welcome = (props) => {
+  return <div>Welcome</div>;
+};
+
+const Nav = (props) => {
+  return (
+    <nav>
+      <ul>
+        <li><NavLink to="/" exact>Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/test/id1">test id1</NavLink></li>
+        <li><NavLink to="/test/id2">test id2</NavLink></li>
+      </ul>
+    </nav>
+  );
+};
+
+const Test = (props) => {
+  return <div> ID: {props.match.params.id} </div>;
+};
+
+const Fallback = (props) => {
+  return <div>URL Not Found</div>;
+};
+
+const App = (props) => {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Welcome} />
+          <Route path="/about" component={About} />
+          <Route exact path="/test/:id" component={Test} />
+          <Route component={Fallback} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('main'));
