a7edbfd29881c6548003fc9646671d7d3072ffcf
nshade12 <nicholas.r.shade.20@dartmouth.edu>
2025-06-16 17:27:04 -0400

    Base bare vite setup

--- src/index.js
+++ src/index.js
@@ -1,23 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
+import $ from 'jquery';
+import './style.scss';
 
-import reducers from './reducers';
+let num = 0;
 
-import App from './components/app';
+setInterval(() => {
+  $('#main').html(`<h1>You've been on this page for ${num} seconds.</h1>`);
+  num += 1;
+}, 1000);
 
-// this creates the store with the reducers, and does some other stuff to initialize devtools
-// boilerplate to copy, don't have to know
-const store = createStore(reducers, {}, compose(
-  applyMiddleware(),
-  window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f,
-));
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+// import { createStore, applyMiddleware, compose } from 'redux';
 
-// we now wrap App in a Provider
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('main'),
-);
+// import reducers from './reducers';
+
+// import App from './components/app';
+
+// // this creates the store with the reducers, and does some other stuff to initialize devtools
+// // boilerplate to copy, don't have to know
+// const store = createStore(reducers, {}, compose(
+//   applyMiddleware(),
+//   window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f,
+// ));
+
+// // we now wrap App in a Provider
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+//   document.getElementById('main'),
+// );
