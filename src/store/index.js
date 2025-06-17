240463abffc1aba45782eb80faf6bd278c2a92fb
nshade12 <nicholas.r.shade.20@dartmouth.edu>
2025-06-17 18:11:22 -0400

    State management done

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,14 @@
 import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
+import { immer } from 'zustand/middleware/immer';
 
-const useStore = create(devtools((set) => {
+const useStore = create(devtools(immer((set) => {
   return {
     // Define your state and actions here
     count: 0,
-    increment: () => set((state) => ({ count: state.count + 1 })),
-    decrement: () => set((state) => ({ count: state.count - 1 })),
+    increment: () => set((draftState) => { draftState.count += 1; }, false, 'count/increment'),
+    decrement: () => set((draftState) => { draftState.count -= 1; }, false, 'count/decrement'),
   };
-}));
+})));
 
 export default useStore;
